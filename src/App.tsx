68675bd087c456c13fed73701e9a8b881b71de3b
OLIX <80951658+OLIX-boop@users.noreply.github.com>
2024-03-07 19:16:49 +0100

    converted tasks into obj and added filter function+

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import BackGround from "./components/background/bg";
 import Header from "./components/header/header";
@@ -7,14 +7,47 @@ import Task from "./components/task/task";
 import Controls from "./components/controls/controls";
 
 function App() {
-  const [Tasks, setTasks] = useState(['ciao', 'noce']);
-
-  const createNewTask = (newMsg:string):void => setTasks([...Tasks, newMsg]);
+  const [Tasks, setTasks] = useState([
+    { task: "Fare la cacca", completed: false },
+    { task: "Allenamento", completed: true },
+  ]);
+  const [filteredTsk, setFilteredStk] = useState({
+    type: "All",
+    tsks: [...Tasks],
+  });
 
   const editMSG = () => {};
 
-  const deleteMSG = (i:number) => setTasks((prevTasks) => prevTasks.filter((_, index) => index !== i));
-  
+  const filterMessages = (type: string) => {
+    // ["All", "Active", "Completed"]
+    if (type === "Active")
+      setFilteredStk({
+        type: "Active",
+        tsks: Tasks.filter((tsk) => !tsk.completed),
+      });
+    else if (type === "Completed")
+      setFilteredStk({
+        type: "Completed",
+        tsks: Tasks.filter((tsk) => tsk.completed),
+      });
+    else setFilteredStk({ type: "All", tsks: [...Tasks] }); // All
+  };
+
+  useEffect(() => {
+    const type = filteredTsk.type;
+    if (type === "Active") setFilteredStk({type: "Active",tsks: Tasks.filter((tsk) => !tsk.completed)});
+    else if (type === "Completed") setFilteredStk({ type: "Completed", tsks: Tasks.filter((tsk) => tsk.completed)});
+    else setFilteredStk({ type: "All", tsks: [...Tasks] });
+  }, [Tasks, filteredTsk.type]);
+
+  const createNewTask = (newMsg: string): void => {
+    setTasks([...Tasks, { task: newMsg, completed: false }]);
+  };
+
+  const deleteMSG = (i: number) => {
+    setTasks((prevTasks) => prevTasks.filter((_, index) => index !== i));
+  };
+
   return (
     <>
       <BackGround />
@@ -27,10 +60,19 @@ function App() {
 
           <div className="todos-container mt-6 bg-elements_black rounded-md">
             <div className="tasks-container overflow-auto max-h-[57vh]">
-              {Tasks.map((e, i) => <Task key={i} msg={e} index={i} edit={editMSG} deleteTsk={deleteMSG} />)}
+              {filteredTsk.tsks.map((e, i) => (
+                <Task
+                  key={i}
+                  msg={e.task}
+                  completed={e.completed}
+                  index={i}
+                  edit={editMSG}
+                  deleteTsk={deleteMSG}
+                />
+              ))}
             </div>
 
-            <Controls tasks={Tasks}/>
+            <Controls tasks={Tasks} filter={filterMessages} />
           </div>
         </div>
       </div>
--- src/components/controls/controls.tsx
+++ src/components/controls/controls.tsx
@@ -1,22 +1,33 @@
 import { useState } from "react";
 
-const Controls = ({tasks}:{tasks:Array<string>}) => {
-    const [currentSelector, setCurrentSelector] = useState("All"); //All, Active, Completed
+const Controls = ({tasks,filter}: {tasks: Array<object>, filter: (type: string) => void}) => {
+  const selectors = ["All", "Active", "Completed"];
+  const style = "hover:text-slate-100 ";
+  const [currentSelector, setCurrentSelector] = useState("All"); //All, Active, Completed
 
-    const style = "hover:text-slate-100 ";
-    const selectors = ["All", "Active", "Completed"];
   return (
     <>
       <div className="flex w-full justify-between p-4">
-        <p className={"text-gray-500 "+style}>{tasks.length} Items left</p>
+        <p className={"text-gray-500 " + style}>{tasks.length} Items left</p>
 
         <div className="flex justify-between gap-2 ">
-            {selectors.map((e, i) => 
-                <p key={i} onClick={() => setCurrentSelector(e)} className={`${style} ${currentSelector === e ? "text-blue-500" : "text-gray-500"}`}>{e}</p>)
-            }
+          {selectors.map((e, i) => (
+            <p
+              key={i}
+              onClick={() => {
+                setCurrentSelector(e);
+                filter(e);
+              }}
+              className={`${style} ${
+                currentSelector === e ? "text-blue-500" : "text-gray-500"
+              }`}
+            >
+              {e}
+            </p>
+          ))}
         </div>
 
-        <p className={"text-gray-500 "+style}>Clear Completed</p>
+        <p className={"text-gray-500 " + style}>Clear Completed</p>
       </div>
     </>
   );
